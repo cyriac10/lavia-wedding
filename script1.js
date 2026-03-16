49d4c619b434e43c9a980d441f48f2d2bb445892
cyriac10 <153482783+cyriac10@users.noreply.github.com>
2026-03-16 11:42:53 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,15 @@
+function openImg(img) {
+    document.getElementById("lightbox").style.display = "flex";
+    document.getElementById("lightbox-img").src = img.src;
+}
+
+function closeImg() {
+    document.getElementById("lightbox").style.display = "none";
+}
+
+function seeAll() {
+    document.querySelectorAll('.hidden').forEach(img => {
+        img.classList.remove('hidden');
+    });
+    document.querySelector('.see-all-btn').style.display = 'none';
+}
